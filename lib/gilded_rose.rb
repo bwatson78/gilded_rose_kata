06bf81bd173c20c592c49b9af175b7c893fbf1ab
bwatson78 <brad.watson.orlando@gmail.com>
2019-03-22 11:54:27 -0400

    wraps up the modularization of the update_quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,31 +2,31 @@ def update_quality(items)
   items.each do |item|
     case item.name
     when 'Aged Brie'
-      if item.sell_in > 0 && item.quality < 50
+      if sell_in_present(item) && qual_less_than_fifty(item)
         add_quality(item, 1) 
-      elsif item.sell_in <= 0 && item.quality <= 48
-        item.quality += 2 
+      elsif sell_in_zero_or_less(item) && qual_less_than_or_eq_max_by(item, 2)
+        add_quality(item, 2)
       else  
         item.quality = 50
       end
       decrease_sell_in_date(item)
     when 'Sulfuras, Hand of Ragnaros'
     when 'Backstage passes to a TAFKAL80ETC concert'
-      if item.sell_in <= 0 
+      if sell_in_zero_or_less(item) 
         item.quality = 0
-      elsif item.sell_in > 0 && item.sell_in <= 10 && item.sell_in > 5 && item.quality <= 48
-        item.quality += 2
-      elsif item.sell_in > 0 && item.sell_in <= 5 && item.quality <= 47
-        item.quality += 3 
-      elsif item.sell_in > 10 && item.quality < 50
-        item.quality += 1
+      elsif backstage_check_for_add_two(item)
+        add_quality(item, 2)
+      elsif backstage_check_for_add_three(item)
+        add_quality(item, 3)
+      elsif item.sell_in > 10 && qual_less_than_fifty(item)
+        add_quality(item, 1)
       end
       decrease_sell_in_date(item)
     when 'Conjured Mana Cake'
-      (item.sell_in > 0 ? item.quality -= 2 :  item.quality -= 4) unless item.quality == 0
+      (sell_in_present(item) ? remove_quality(item, 2) : remove_quality(item, 4)) unless qual_zero(item)
       decrease_sell_in_date(item)
     else
-      (item.sell_in > 0 ? item.quality -= 1 :  item.quality -= 2) unless item.quality == 0
+      (sell_in_present(item) ? remove_quality(item, 1) : remove_quality(item, 2)) unless qual_zero(item)
       decrease_sell_in_date(item)
     end
   end
@@ -38,6 +38,46 @@ end
 
 def add_quality(item, inc)
   item.quality += inc 
+end
+
+def remove_quality(item, dec)
+  item.quality -= dec
+end
+
+def qual_zero(item)
+  item.quality == 0
+end
+
+def sell_in_present(item)
+  item.sell_in > 0
+end
+
+def qual_less_than_fifty(item)
+  item.quality < 50
+end
+
+def sell_in_less_than_or_eq(item, num)
+  item.sell_in <= num
+end
+
+def sell_in_zero_or_less(item)
+  sell_in_less_than_or_eq(item, 0)
+end
+
+def max_qual
+  50
+end
+
+def qual_less_than_or_eq_max_by(item, num)
+  item.quality <= (max_qual - num)
+end
+
+def backstage_check_for_add_two(item)
+  sell_in_present(item) && sell_in_less_than_or_eq(item, 10) && item.sell_in > 5 && qual_less_than_or_eq_max_by(item, 2)
+end
+
+def backstage_check_for_add_three(item)
+  sell_in_present(item) && sell_in_less_than_or_eq(item, 5) && qual_less_than_or_eq_max_by(item, 3)
 end
 #----------------------------
 # DO NOT CHANGE THINGS BELOW
