303819e3deaf24aaff60f2f6a125e75a06a0f0e5
bwatson78 <brad.watson.orlando@gmail.com>
2019-03-22 11:26:12 -0400

    makes all tests pass, starts modularization

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,22 +3,42 @@ def update_quality(items)
     case item.name
     when 'Aged Brie'
       if item.sell_in > 0 && item.quality < 50
-        item.quality += 1 
+        add_quality(item, 1) 
       elsif item.sell_in <= 0 && item.quality <= 48
         item.quality += 2 
       else  
         item.quality = 50
       end
-      item.sell_in -= 1 
+      decrease_sell_in_date(item)
     when 'Sulfuras, Hand of Ragnaros'
-    when 
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      if item.sell_in <= 0 
+        item.quality = 0
+      elsif item.sell_in > 0 && item.sell_in <= 10 && item.sell_in > 5 && item.quality <= 48
+        item.quality += 2
+      elsif item.sell_in > 0 && item.sell_in <= 5 && item.quality <= 47
+        item.quality += 3 
+      elsif item.sell_in > 10 && item.quality < 50
+        item.quality += 1
+      end
+      decrease_sell_in_date(item)
+    when 'Conjured Mana Cake'
+      (item.sell_in > 0 ? item.quality -= 2 :  item.quality -= 4) unless item.quality == 0
+      decrease_sell_in_date(item)
     else
       (item.sell_in > 0 ? item.quality -= 1 :  item.quality -= 2) unless item.quality == 0
-      item.sell_in -= 1 
+      decrease_sell_in_date(item)
     end
   end
 end
 
+def decrease_sell_in_date(item)
+  item.sell_in -= 1 
+end
+
+def add_quality(item, inc)
+  item.quality += inc 
+end
 #----------------------------
 # DO NOT CHANGE THINGS BELOW
 #----------------------------
